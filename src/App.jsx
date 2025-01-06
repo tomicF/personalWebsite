b2c0759ecccf3e2da64c380e11dd0a739499ae90
tomicF <filetomic2008@gmail.com>
2025-01-06 21:40:12 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+
+function Model(props) {
+  const { scene } = useGLTF("/bmw.glb");
+  return <primitive object={scene} {...props} />;
+}
+
+function App() {
+  return (
+    <Canvas
+      dpr={[1, 2]}
+      shadows
+      camera={{ fov: 45 }}
+      style={{ position: "absolute" }}
+    >
+      <color attach="background" args={["#101010"]} />
+      <PresentationControls
+        speed={1.5}
+        global
+        zoom={0.5}
+        polar={[-0.1, Math.PI / 4]}
+      >
+        <Stage environment={"city"}>
+          <Model scale={0.01} />
+        </Stage>
+      </PresentationControls>
+    </Canvas>
+  );
+}
+
+export default App;
