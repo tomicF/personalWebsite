8f17f56c12996b43a9b4725f052aed6e360146b1
tomicF <filetomic2008@gmail.com>
2025-01-07 12:06:10 +0100

    Added a 3d model and a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,50 @@
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+import "./App.css";
 
 function Model(props) {
-  const { scene } = useGLTF("/bmw.glb");
+  const { scene } = useGLTF("/devLogo.glb");
   return <primitive object={scene} {...props} />;
 }
 
 function App() {
   return (
-    <Canvas
-      dpr={[1, 2]}
-      shadows
-      camera={{ fov: 45 }}
-      style={{ position: "absolute" }}
-    >
-      <color attach="background" args={["#101010"]} />
-      <PresentationControls
-        speed={1.5}
-        global
-        zoom={0.5}
-        polar={[-0.1, Math.PI / 4]}
+    <>
+      <div>
+        <p className="myNameIs">my name is</p>
+        <p className="myName">Filip Tomic</p>
+        <p className="mydescription">
+          I’m a high school student and aspiring developer from Sarajevo. I
+          enjoy building projects with code. My goal is to inspire others
+          through tech and contribute to education in Bosnia and Herzegovina.
+          Let’s build something awesome! 🚀
+        </p>
+      </div>
+
+      <Canvas
+        dpr={[1, 2]}
+        camera={{ fov: 45 }}
+        style={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          width: "60%",
+          height: "100%",
+        }}
       >
-        <Stage environment={"city"}>
-          <Model scale={0.01} />
-        </Stage>
-      </PresentationControls>
-    </Canvas>
+        <color attach="background" args={["#000000"]} />
+        <PresentationControls
+          speed={1.5}
+          global
+          zoom={0.5}
+          polar={[-0.1, Math.PI / 4]}
+        >
+          <Stage environment={"city"} shadows={false}>
+            <Model scale={0.01} />
+          </Stage>
+        </PresentationControls>
+      </Canvas>
+    </>
   );
 }
 
